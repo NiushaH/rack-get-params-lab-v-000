5005b58b6d1226840aa4280e585f329c06ef2d6f
Niusha H <niusha.hadziomerovic@gmail.com>
2019-08-19 00:58:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -19,10 +19,8 @@ class Application
         resp.write "#{item_in_cart}\n"
       end
     elsif req.path.match(/add/)
-      add = GET
-        resp.write "#{add}\n"
-      end 
-
+      add = 
+      resp.write GET"#{item}\n"
     else
       resp.write "Path Not Found"
     end
